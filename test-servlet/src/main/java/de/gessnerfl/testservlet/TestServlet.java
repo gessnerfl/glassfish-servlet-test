aff0dd3c726464c3020cf36001ad007d9d9b1f13
Florian Gessner <florian.gessner@tis.biz>
2017-12-28 21:06:44 +0100

    Add headers to servlet

--- test-servlet/src/main/java/de/gessnerfl/testservlet/TestServlet.java
+++ test-servlet/src/main/java/de/gessnerfl/testservlet/TestServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Enumeration;
 
 @WebServlet(urlPatterns = "/test")
 public class TestServlet extends HttpServlet{
@@ -14,6 +15,9 @@ public class TestServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Writer writer = resp.getWriter();
+        writer.append("\n------------------------------------------------------\n");
+        writer.append("Request Parameters:");
+        writer.append("\n------------------------------------------------------\n");
         writer.append("Request URL: "+req.getRequestURL().toString()+"\n");
         writer.append("Request URI: "+req.getRequestURI()+"\n");
         writer.append("Server Name: "+req.getServerName()+"\n");
@@ -21,7 +25,16 @@ public class TestServlet extends HttpServlet{
         writer.append("Local Name: "+req.getLocalName()+"\n");
         writer.append("Local Port: "+req.getLocalPort()+"\n");
         writer.append("Remote Host: "+req.getRemoteHost()+"\n");
-        writer.append("Remote Port: "+req.getRemotePort());
+        writer.append("Remote Port: "+req.getRemotePort()+"\n");
+        writer.append("\n\n------------------------------------------------------\n");
+        writer.append("Headers:");
+        writer.append("\n------------------------------------------------------\n");
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while(headerNames.hasMoreElements()){
+            String key = headerNames.nextElement();
+            String value = req.getHeader(key);
+            writer.append("   "+key+": "+value+"\n");
+        }
         writer.flush();
     }
 }
